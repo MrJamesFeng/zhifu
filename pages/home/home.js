6e5ba7937b4260459d39762249e6395cb6789935
ldy <ldy@ldydeiMac.local>
2018-02-24 18:01:06 +0800

    first

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,20 +1,55 @@
 // pages/home/home.js
+var util = require("../../utils/util.js")
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    currentTab:0,
+    // hideLoading:false
+    // stories:[],
+    others:[],
+    top_stories:[],
+    screenHeight:0,
+    screenWidth:0,
+    dataList:[],
+    dataListDateCurrent:0,
+    dataListDateCount:0
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
-  },
 
+    wx.getSystemInfo({
+      success: (res) => { this.setData({ screenHeight: res["screenHeight"], screenWidth: res["screenWidth"]})},
+    })
+
+    // 精选
+    util.AJAX("news/latest",(res)=>{
+      console.log("精选")
+      console.log(res)
+      var dateData = {}
+      var dataListDateCurrent = res["date"]
+      dateData["dateDay"] = util.getFormatDate(dataListDateCurrent)
+      dateData["stories"] = res["stories"]
+      this.setData({ top_stories: res["top_stories"], dataList: [dateData]})
+      // console.log(this.data)
+      
+    })
+
+    // 日报
+    util.AJAX("themes", (res)=> {
+      console.log("日报")
+      console.log(res)
+      this.setData({ others: res["others"]})
+      // console.log(this.data)
+
+    });
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -62,5 +97,25 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+
+  changeType:function(e){
+    var selectedType = e.currentTarget.dataset.type
+    var currentTab = this.data.currentTab
+    if(selectedType == currentTab){
+      return
+    }
+
+    this.setData({ currentTab: selectedType})
+  },
+
+  
+  scrolltolower:function(e){ 
+
+    util.AJAX("news/before/" + currentDate, function (res) {
+      console.log("news/before/")
+      console.log(res)
+    })
   }
+  
 })
